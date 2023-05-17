07c89afe0eb5be8f0f3d0e24084f856dd7388f7f
Showman_Das <Showmandas3216@gmail.com>
2023-05-18 02:21:48 +0600

    add google font

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -15,11 +15,11 @@ export default function Gallery() {
           <img src={gallery_1} alt="toy gallery pic" className='img-fluid w-75' />
         </div>
         <div className="col-lg-4 col-12 text-center mb-5">
-        <img src={gallery_2} alt="toy gallery pic" className='img-fluid w-75' />
+        <img src={gallery_2} alt="toy gallery pic" className='img-fluid w-75 h-100' />
 
         </div>
         <div className="col-lg-4 col-12 text-center mb-5">
-        <img src={gallery_3} alt="toy gallery pic" className='img-fluid w-75' />
+        <img src={gallery_3} alt="toy gallery pic" className='img-fluid w-75 h-100' />
 
         </div>
         <div className="col-lg-4 col-12 text-center mb-5">
@@ -27,7 +27,7 @@ export default function Gallery() {
 
         </div>
         <div className="col-lg-4 col-12 text-center mb-5">
-        <img src={gallery_5} alt="toy gallery pic" className='img-fluid w-75' />
+        <img src={gallery_5} alt="toy gallery pic" className='img-fluid w-75 h-100' />
 
         </div>
         <div className="col-lg-4 col-12 text-center mb-5">
